ed367f5376ae093571a0ddb4a02b70c864f614ba
gzl <wgguozhilong@163.cpm>
2018-11-26 21:50:31 +0800

    init project+1

--- src/test/java/com/abin/easyuidemo/UserTest.java
+++ src/test/java/com/abin/easyuidemo/UserTest.java
@@ -3,25 +3,17 @@ package com.abin.easyuidemo;
 import com.abin.easyuidemo.dao.UserDao;
 import com.abin.easyuidemo.entity.User;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class EasyuidemoApplicationTests extends TmallApplicationTests {
-
+public class UserTest extends TmallApplicationTests {
     @Autowired
     UserDao userMapper;
 
     @Test
-    public void contextLoads() {
+    public void name() {
         User user = userMapper.selectByPrimaryKey(12);
-        System.out.println("----");
-
-
         System.out.println(user);
     }
+
 
 }
