2bc23033d8c91393bf7de989626440ce92540e67
gzl <wgguozhilong@163.cpm>
2018-11-26 18:58:41 +0800

    init project

---
+++ src/main/java/com/abin/easyuidemo/EasyuidemoApplication.java
@@ -0,0 +1,12 @@
+package com.abin.easyuidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyuidemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyuidemoApplication.class, args);
+    }
+}
